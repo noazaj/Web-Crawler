bcee7c0cb9bb0aca2e99753fde5e98d4c7f67e73
noazaj <noahzajicek@gmail.com>
2024-09-30 00:07:42 -0400

    Finish getting urls from HTML pages

--- get_urls_fromHTML.go
+++ get_urls_fromHTML.go
@@ -2,24 +2,36 @@ package main
 
 import (
 	"fmt"
+	"net/url"
 	"strings"
 
 	"golang.org/x/net/html"
 )
 
 func GetURLsFromHTML(htmlBody, rawBaseURL string) ([]string, error) {
+	baseURL, err := url.Parse(rawBaseURL)
+	if err != nil {
+		return nil, fmt.Errorf("couldn't parse base URL: %v", err)
+	}
+
 	doc, err := html.Parse(strings.NewReader(htmlBody))
 	if err != nil {
 		return []string{}, err
 	}
 	
+	var urls []string
 	var f func(*html.Node)
 	f = func(n *html.Node) {
 		if n.Type == html.ElementNode && n.Data == "a" {
 			for _, a := range n.Attr {
 				if a.Key == "href" {
-					fmt.Println(a.Val)
-					break
+					href, err := url.Parse(a.Val)
+					if err != nil {
+						fmt.Printf("couldn't parse href '%v': %v\n", a.Val, err)
+						continue
+					}
+					resolvedURL := baseURL.ResolveReference(href)
+					urls = append(urls, resolvedURL.String())
 				}
 			}
 		}
@@ -29,5 +41,5 @@ func GetURLsFromHTML(htmlBody, rawBaseURL string) ([]string, error) {
 	}
 	f(doc)
 
-	return []string{}, nil
+	return urls, nil
 }
