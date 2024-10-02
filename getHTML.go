352be0279e3c2164beafdba784488f7a34dc217d
Noah Zajicek <nzajicek@WVWAUTHORITY.COM>
2024-10-02 11:35:19 -0400

    Implement a helper function to get HTML body for main program

---
+++ getHTML.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"io"
+	"log"
+	"net/http"
+)
+
+func GetHTML(rawURL string) (string, error) {
+	resp, err := http.Get(rawURL)
+	if err != nil {
+		log.Fatalf("error making 'GET' request to URL: %s", rawURL)
+		return "", err
+	}
+	defer resp.Body.Close()
+
+	rawHTML, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal("error reading response body")
+		return "", err
+	}
+
+	return string(rawHTML), nil
+}
--- main.go
+++ main.go
@@ -2,8 +2,41 @@ package main
 
 import (
 	"fmt"
+	"os"
 )
 
 func main() {
-	fmt.Println("Hello World")
+	// Ensure there's exactly one URL argument
+	if len(os.Args) < 2 {
+		fmt.Println("no website provided")
+		os.Exit(1)
+	} else if len(os.Args) > 2 {
+		fmt.Println("too many arguments provided")
+		os.Exit(1)
+	}
+
+	// Get the URL argument
+	url := os.Args[1]
+
+	fmt.Println("starting crawl")
+	fmt.Println(url)
+
+	// Get the HTML body
+	rawHTML, err := GetHTML(url)
+	if err != nil {
+		fmt.Printf("error getting HTMl body: %v", err)
+		os.Exit(1)
+	}
+
+	// Parse the URLs from the HTML body
+	urls, err := GetURLsFromHTML(rawHTML, url)
+	if err != nil {
+		fmt.Printf("Error parsing URLs from HTML: %v\n", err)
+		os.Exit(1)
+	}
+
+	// Print the extracted URLs
+	for _, extractedURL := range urls {
+		fmt.Println(extractedURL)
+	}
 }
